a942292517bbbb26042e1a011e361429183531ea
Fusion86 <fusion86@cerbus.nl>
2019-09-26 15:19:51 +0200

    We need more tunes

--- week2_6_due/generator.cpp
+++ week2_6_due/generator.cpp
@@ -8,6 +8,8 @@ const char let_it_be[] = "Beatles Let It Be:d=4,o=5,b=100:16e6,8d6,c6,16e6,8g6,8
 const char muppets[] = "Muppets:d=4,o=5,b=250:c6,c6,a,b,8a,b,g,p,c6,c6,a,8b,8a,8p,g.,p,e,e,g,f,8e,f,8c6,8c,8d,e,8e,8e,8p,8e,g,2p,c6,c6,a,b,8a,b,g,p,c6,c6,a,8b,a,g.,p,e,e,g,f,8e,f,8c6,8c,8d,e,8e,d,8d,c";
 const char rickroll[] = "Never gonna give you up:d=4,o=5,b=120:16c,16d,16f,16d,16a.,16p,32p,8a,16p,g.,16c,16d,16f,16d,16g.,16p,32p,8g,16p,8f.,16e,8d,16c,16d,16f,16d,f,8g,8e.,16d,8c,8c4,8c,8g,8p,2f,16c,16d,16f,16d,16a.,16p,32p,8a,16p,g.,16c,16d,16f,16d,c6,8e,8f.,16e,8d,16c,16d,16f,16d,f,8g,8e.,16d,8c,8c4,8c,8g,8p,2f";
 const char one[] = "NumberOne:d=16,o=5,b=168:4f.,8c6,16b5,16c6,16b5,16c6,8b5,8c6,4g#5,4f.,8f,8g#5,8c6,4c#6,4g#5,4c#6,4d#6,8c6,8c#6,8c6,8c#6,2c6";
+const char pirates[] = "ThemePi:d=4,o=6,b=200:8d,8e,2f,8g,8a,g,f,e,f,g,a,g,p,8f,8g,a,p,8g,8f,e,f,e,d,p,8e,8c,d,8p,p,8d,8e,f,p,8e,8f,g,f,g,a,g,f,d";
+const char tetris[] = "Tetris:d=4,o=5,b=160:e6,8b,8c6,8d6,16e6,16d6,8c6,8b,a,8a,8c6,e6,8d6,8c6,b.,8c6,d6,e6,c6,a,2a,8p,d6,8f6,a6,8g6,8f6,e.6,8c6,e6,8d6,8c6,b,8b,8c6,d6,e6,c6,a,2a";
 
 int main(int argc, char **argv) {
     if (argc < 2) {
@@ -20,7 +22,7 @@ int main(int argc, char **argv) {
 
     auto player = notes_to_cpp(f);
     player.start();
-    rtttl_play(player, sos);
+    rtttl_play(player, tetris);
     player.end();
 
     f.close();

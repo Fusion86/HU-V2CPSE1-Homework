ee4809cb6fa1fdad89c24bc49b14d3b0ed20fe7a
Fusion86 <fusion86@cerbus.nl>
2019-11-13 12:42:39 +0100

    Tijd voor een andere opleiding

--- week4_decoder_in_asm/decode_str_compressed.asm
+++ week4_decoder_in_asm/decode_str_compressed.asm
@@ -1,43 +1,81 @@
 .cpu cortex-m0
 .align 2
 
+.data
 
-.bss
-.lcomm buffer, 100
-
+buffer:
+    .skip 40
+    .byte 0
 
 .text
 .global decode_str_compressed
-@ void decode_str_compressed()
-@ ----
-@ r0 = read() puts read character here
-@ r4 = ptr to the next character that should be read
-@ ----
 decode_str_compressed:
-    push { lr }
-    ldr r4, =str_compressed
+	push { r4, r5, r6, lr }
+	ldr r4, =str_compressed
+
+loop:
+	ldrb r0, [ r4 ]             @ Read char
+
+    cmp r0, #0                  @ Goto exit if char == \0
+    beq done
+
+	cmp r0, #64                 @ Goto decompress if char == '@'
+	beq decompress
+
+	bl add_buffer               @ Buffer stuff
+	bl uart_put_char            @ Print character
+
+    add r4, r4, #1              @ Increment ptr
+    b loop
 
 decompress:
-    bl read                     @ Read next character
-    add r0, #0                  @ If character == \0
-    beq done                    @ Return
-    @ TODO
-    bl add_char
-    b decompress
+	add r4, #1                  @ Skip '@' token
+
+	@ Store token offset
+	ldrb r5, [r4]
+	sub r5, #'0'
+	ldr r0, =buffer
+	add r5, r0
+
+	@ Store token size
+	add r4, #1
+	ldrb r6, [r4]
+	sub r6, #'0'
+
+    @ Skip size character
+	add r4, #1
+
+sub_loop:
+    @ If size == 0 go back to main loop
+	cmp r6, #0
+	beq loop
+
+    @ Subtract size and load char
+	sub r6, #1
+	ldrb r0, [r5]
+
+    @ Buffer stuff and print character
+	bl add_buffer
+	bl uart_put_char
+	b sub_loop                  @ Resume this loop
 
 done:
-    pop { pc }
+   pop { r4, r5, r6, pc }       @ Return
 
 
-@ char read()
-read:
-    ldr r0, [r4]
-    add r4, r4, #1
-    mov pc, lr
+add_buffer:
+    push { r4, lr }
+    ldr r1, =buffer
+    mov r4, r1
+    add r4, #39                 @ 39 is the last index in the buffer
 
+ buffer_loop:
+    ldrb r2, [r4]
+    strb r2, [r4, #1]
 
-@ void add(char c)
-add_char:
-    push { lr }
-    bl uart_put_char
-    pop { pc }
+    sub r4, #1                  @ Subtract 1 from buffer index
+    cmp r4, r1                  @ If buffer index is not 0
+    bge buffer_loop             @ Loop again
+
+    strb r0, [r1]
+    pop { r4, pc }              @ Exit
